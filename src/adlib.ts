7ee6ca73eceb2b2a55ddf268ff8535ca63917996
Ryan Baxley <ryan.baxley1@gmail.com>
2018-01-31 20:22:56 -0500

    Changes

--- src/adlib.ts
+++ src/adlib.ts
@@ -1,10 +1,25 @@
 import { VoiceConnection } from 'discord.js';
+import { ps } from 'pocketsphinx';
+
+const modeldir = '../models';
+
+var config = new ps.Decoder.defaultConfig();
+config.setString("-hmm", modeldir + "en-us");
+config.setString("-dict", modeldir + "cmudict-en-us.dict");
+config.setString("-lm", modeldir + "en-us.lm.bin");
+const decoder = new ps.Decoder(config);
 
 export const onConnection = (con: VoiceConnection) => {
   console.log('connected');
   const recv = con.createReceiver();
+  decoder.startUtt();
   recv.on('opus', (user, buf) => {
     console.log(user.username);
     console.log(buf.length);
+    decoder.processRaw(buf, false, false);
+    const it = decoder.seg().iter();
+    for(let seg of it) {
+      console.log(seg.word, seg.startFrame, seg.endFrame);
+    }
   })
 }
