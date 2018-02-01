863622c5bd1c4c98539c8203833a4d54190aa49b
Ryan Baxley <ryan.baxley1@gmail.com>
2018-01-31 20:46:31 -0500

    Changes

--- src/adlib.ts
+++ src/adlib.ts
@@ -1,27 +1,25 @@
 import { VoiceConnection } from 'discord.js';
-// import { ps } from 'pocketsphinx';
-// import Sonus from 'sonus';
-const Sonus = require('sonus');
-const hotwords = [
-  { file: 'models/Hello.pmdl', hotword: 'hello' }
-];
-const language = 'en-US';
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
-eventEmitter.on('error', () => console.log('error'));
-eventEmitter.on('final-result', () => console.log('final-result'));
-eventEmitter.on('partial-result', () => console.log('partial-result'));
-eventEmitter.on('hotword', () => console.log('hotword'));
-eventEmitter.on('silence', () => console.log('silence'));
-eventEmitter.on('sound', () => console.log('sound'));
-eventEmitter.on('error', () => console.log('error'));
-const sonus = Sonus.init({ hotwords, language, recordProgram: 'arecord' }, eventEmitter)
+import { ps } from 'pocketsphinx';
+
+const modeldir = '../models';
+
+var config = new ps.Decoder.defaultConfig();
+// config.setString("-hmm", modeldir + "en-us");
+// config.setString("-dict", modeldir + "cmudict-en-us.dict");
+// config.setString("-lm", modeldir + "en-us.lm.bin");
+const decoder = new ps.Decoder(config);
 
 export const onConnection = (con: VoiceConnection) => {
   console.log('connected');
   const recv = con.createReceiver();
+  decoder.startUtt();
   recv.on('opus', (user, buf) => {
     console.log(user.username);
     console.log(buf.length);
+    decoder.processRaw(buf, false, false);
+    const it = decoder.seg().iter();
+    for(let seg of it) {
+      console.log(seg.word, seg.startFrame, seg.endFrame);
+    }
   })
 }
